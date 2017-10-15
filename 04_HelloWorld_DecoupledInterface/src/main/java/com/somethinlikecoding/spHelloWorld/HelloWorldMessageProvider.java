ae09c176b881ce2d3e99a20e7d5474d4157d71fd
umercis <umercis@gmail.com>
2017-10-15 07:50:49 +0000

    Committing Hello World Example 4

---
+++ 04_HelloWorld_DecoupledInterface/src/main/java/com/somethinlikecoding/spHelloWorld/HelloWorldMessageProvider.java
@@ -0,0 +1,10 @@
+package com.somethinlikecoding.spHelloWorld;
+
+public class HelloWorldMessageProvider implements MessageProvider {
+
+    public String getMessage() {
+
+        return "Hello World!";
+    }
+
+}
