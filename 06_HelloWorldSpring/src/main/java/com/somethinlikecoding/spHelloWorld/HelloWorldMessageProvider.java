18b814c393a15b9a0ce0164f0571e9706d33bc92
umercis <umercis@gmail.com>
2017-10-15 08:46:33 +0000

    Committing Hello World Example 6 and project strutures

---
+++ 06_HelloWorldSpring/src/main/java/com/somethinlikecoding/spHelloWorld/HelloWorldMessageProvider.java
@@ -0,0 +1,9 @@
+package com.somethinlikecoding.spHelloWorld;
+
+public class HelloWorldMessageProvider implements MessageProvider {
+
+    public String getMessage() {
+        return "Hello World!";
+    }
+
+}
---
+++ 06_HelloWorldSpring/src/main/java/com/somethinlikecoding/spHelloWorld/StandardOutMessageRenderer.java
@@ -0,0 +1,21 @@
+package com.somethinlikecoding.spHelloWorld;
+
+public class StandardOutMessageRenderer implements MessageRenderer {
+
+    private MessageProvider messageProvider = null;
+
+    public void render() {
+        if (messageProvider == null) {
+            throw new RuntimeException(
+                    "You must set the property messageProvider of class:"
+                            + StandardOutMessageRenderer.class.getName());
+        }
+
+        System.out.println(messageProvider.getMessage());
+    }
+
+    public void setMessageProvider(MessageProvider provider) {
+        this.messageProvider = provider;
+    }
+
+}
